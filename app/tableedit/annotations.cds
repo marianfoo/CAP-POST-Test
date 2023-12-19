9aaff03318a14af2b694c031e510ef189405290c
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2023-12-19 14:14:53 +0100

    add editmode

--- app/tableedit/annotations.cds
+++ app/tableedit/annotations.cds
@@ -1,1 +1,21 @@
 using AdminService as service from '../../srv/admin-service';
+
+annotate AdminService.Books with @(UI: {LineItem: [
+    {
+        Label: 'Title',
+        Value: title
+    },
+    {
+        Label: 'Description',
+        Value: descr
+    },
+    {
+        Label: 'Price',
+        Value: price
+    },
+    {
+        Label: 'Stock',
+        Value: stock
+    }
+] }
+);
--- app/tableedit/webapp/controller/MainView.controller.js
+++ app/tableedit/webapp/controller/MainView.controller.js
@@ -1,15 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("ui5.tableedit.controller.MainView", {
             onInit: function () {
+                this.getView().setModel(new JSONModel({instantSave: false}), "viewData");
 
-            }
+            },
+            onAfterRendering: function() {
+                // set editmode false on startup
+                const smartTable = this.byId("smartTable");
+                smartTable.setEditable(false);
+            },
+            onFieldChange: function(evt) {
+                const viewData = this.getView().getModel("viewData").getData();
+                var change = evt.getParameter("changeEvent");
+                if (change && viewData.instantSave) {
+                  change.getSource().getModel().submitChanges()
+                }
+              },
+              saveAll: function() {
+                this.byId("smartTable").getModel().submitChanges()
+                this.byId("smartTable").setEditable(false);
+              },
+              resetAll: function() {
+                this.byId("smartTable").getModel().resetChanges()
+              },
         });
     });
